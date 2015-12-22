8fe2a65cc64b7ac28c448d631233cc44505416b7
John Ortiz Ordoñez <johnortizo@outlook.com>
2015-12-22 17:15:46 -0500

    App finished

---
+++ app/src/main/java/co/ortizol/ademocalculator/model/Calculator.java
@@ -0,0 +1,7 @@
+package co.ortizol.ademocalculator.model;
+
+/**
+ * Created by johno on 12/21/2015.
+ */
+public class Calculator {
+}
---
+++ app/src/main/java/co/ortizol/ademocalculator/util/Utility.java
@@ -0,0 +1,7 @@
+package co.ortizol.ademocalculator.util;
+
+/**
+ * Created by johno on 12/21/2015.
+ */
+public class Utility {
+}
